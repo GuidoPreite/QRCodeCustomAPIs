d0301f0a70446d42401c244be903b9f0a1230f8a
Guido Preite <43754988+GuidoPreite@users.noreply.github.com>
2024-02-25 19:44:18 +0100

    Add files via upload

---
+++ QRCodeCustomAPIs.Plugins/GenerateQRCodeUrl.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using static QRCodeCustomAPIs.Plugins.PayloadGenerator;
+
+namespace QRCodeCustomAPIs.Plugins
+{
+    public class GenerateQRCodeUrl : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            string url = context.InputParameters["Url"] as string;
+            bool success = false;
+            string base64Result = "";
+            try
+            {
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                Url payloadUrl = new Url(url);
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(payloadUrl);
+                PngByteQRCode png = new PngByteQRCode(qrCodeData);
+                byte[] byteResult = png.GetGraphic(20);
+                base64Result = Convert.ToBase64String(byteResult);
+                success = true;
+            }
+            catch { }
+            context.OutputParameters["Success"] = success;
+            context.OutputParameters["Base64Result"] = base64Result;
+        }
+    }
+}
